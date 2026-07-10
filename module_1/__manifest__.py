94648647b24864160080059aabca7542dbe3ed05
Ickab <ic.tecnology2020@gmail.com>
2026-07-09 19:12:08 -0600

    [FIX] refine warehouse availability layout

--- module_1/__manifest__.py
+++ module_1/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Product Qty Warehouse',
-    'version': '18.0.1.0.3',
+    'version': '18.0.1.0.4',
     'summary': 'Muestra disponibilidad libre de productos por almacen',
     'description': 'Disponibilidad de producto por almacen para Odoo 18.',
     'author': 'IcTechnologyMx',
