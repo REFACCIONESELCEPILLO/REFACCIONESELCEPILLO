4a6f701157c98c2a7bb73e12dc8717fad6b66d9a
Ickab <ic.tecnology2020@gmail.com>
2026-07-16 14:53:01 -0600

    [FIX] use motorcycle brand catalog for OEM

--- sh_auto_part_vehicle/models/vehicle_eom.py
+++ sh_auto_part_vehicle/models/vehicle_eom.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 # Part of Softhealer Technologies.
-from odoo import api, models, fields
+from odoo import models, fields
 
 
 class ShVehicleOEM(models.Model):
@@ -9,13 +9,8 @@ class ShVehicleOEM(models.Model):
 
     name = fields.Char('Code', required=True)
     supplier_id = fields.Many2one(
-        'res.partner',
+        'motorcycle.brand',
         string="Marca",
-        domain="[('category_id', 'in', oem_brand_category_ids)]",
-    )
-    oem_brand_category_ids = fields.Many2many(
-        'res.partner.category',
-        compute='_compute_oem_brand_category_ids',
     )
     is_visible_website = fields.Boolean('Is visible on website?')
     product_id = fields.Many2one('product.template', string='Product')
@@ -27,16 +22,6 @@ class ShVehicleOEM(models.Model):
         'website',
         string='Website'
     )
-
-    @api.depends()
-    def _compute_oem_brand_category_ids(self):
-        category = self.env.ref(
-            'sh_auto_part_vehicle.res_partner_category_oem_brand',
-            raise_if_not_found=False,
-        )
-        for line in self:
-            line.oem_brand_category_ids = category
-
 
 class ShProductSpecification(models.Model):
     _name = "sh.product.specification"
