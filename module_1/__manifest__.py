d39e4da989c056fe635713cb48e4490c225cf004
Ickab <ic.tecnology2020@gmail.com>
2026-07-16 18:07:38 -0600

    [FIX] aisla módulo de ticket

--- module_1/__manifest__.py
+++ module_1/__manifest__.py
@@ -1,16 +1,13 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Diseño de Ticket El Cepillo',
-    'version': '18.0.2.1.0',
+    'version': '18.0.2.2.0',
     'summary': 'Diseño térmico para órdenes de venta',
     'description': 'Personaliza exclusivamente el ticket de orden de venta de El Cepillo.',
     'author': 'IcTechnologyMx',
     'license': 'LGPL-3',
     'category': 'Sales/Sales',
     'depends': [
-        # Garantiza que el reemplazo de inventario se instale antes de
-        # retirar las vistas legadas durante la migración de module_1.
-        'product_qty_warehouse',
         'sale',
     ],
     'data': [
@@ -21,6 +18,7 @@
             'module_1/static/src/scss/sale_ticket_report.scss',
         ],
     },
+    'auto_install': False,
     'application': False,
     'installable': True,
 }
