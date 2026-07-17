36412bf51855f975d34ead30c77e5dfa70c55a68
Ickab <ic.tecnology2020@gmail.com>
2026-07-16 18:16:39 -0600

    [FIX] renombra módulo de ticket

--- module_1/__manifest__.py
+++ module_1/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
-    'name': 'Diseño de Ticket El Cepillo',
-    'version': '18.0.2.2.0',
+    'name': 'design_ticket_sale',
+    'version': '18.0.2.2.1',
     'summary': 'Diseño térmico para órdenes de venta',
     'description': 'Personaliza exclusivamente el ticket de orden de venta de El Cepillo.',
     'author': 'IcTechnologyMx',
